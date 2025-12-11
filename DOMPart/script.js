2f6374a7ffd938da614b36db9adc46231f29a141
Nilesh Patil <nileshpatil6021@gmail.com>
2025-12-11 22:03:04 +0530

    domfile

---
+++ DOMPart/script.js
@@ -0,0 +1,13 @@
+console.log("Hello from script.js!");
+console.log(window);
+console.log(document);
+console.log(document.getElementsByName("p"));
+console.log(document.getElementsByTagName("p"));
+console.log(document.getElementsByClassName("p"));
+console.log(document.getElementById("p1")); 
+alert("Hello from script.js!");
+
+
+
+
+document.query
